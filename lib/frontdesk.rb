90852fdd5d27f3267825aba3b63835c6a2327743
Anne <annehwatson@gmail.com>
2018-03-05 16:24:38 -0800

    view_reservations_list(date) method in frontdesk

--- lib/frontdesk.rb
+++ lib/frontdesk.rb
@@ -1,4 +1,4 @@
-# require "Date"
+require "Date"
 
 require_relative "reservation"
 
@@ -11,9 +11,14 @@ module Hotel
       @reservations = []
     end
 
+    def find_available_room(start_date, end_date)
+      #return first available room that is open for all of the dates passed in to reserve_room
+
+    end
+
     def reserve_room(start_date, end_date, is_block =false)
       reservation_data = {
-        #room number,
+        #room_number: find_available_room,
         start_date: Date.parse(start_date),
         end_date: Date.parse(end_date),
         cost_per_night: unless is_block == true
@@ -30,15 +35,15 @@ module Hotel
     end
 
     def view_reservations_list(date)
-      reservations = @reservations.select{ |reservation| reservation.date_range.include?(date)}
-      puts "I am the view reservations list method: "
+      date_query = Date.parse(date)
+      reservations = @reservations.select{ |reservation| reservation.date_range.include?(date_query)}
       return reservations
     end
 
   end
 end
 
-# my_desk = Hotel::FrontDesk.new
-# my_desk.reserve_room("2017-08-01", "2017-08-02")
-# my_desk.reserve_room("2017-09-01", "2017-09-02")
-# p my_desk.view_reservations_list("2017-09-01")
+my_desk = Hotel::FrontDesk.new
+my_desk.reserve_room("2017-08-01", "2017-08-02")
+my_desk.reserve_room("2017-09-01", "2017-09-02")
+p my_desk.view_reservations_list("2017-09-01")
